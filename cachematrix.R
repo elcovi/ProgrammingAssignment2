6d2e6b9487dbf8c8fab15bb3f8b430b855e29989
elcovi <2coviella@gmail.com>
2015-10-25 09:12:28 -0400

    cachematrix solved & edited
    ELCOVI implementation of R Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+##---------------------------------------------------------
+## 'makeCacheMatrix', takes a matrix as its argument and creates a list object 
+##  with four functions: 
+
+##  1) 'set(y)', a function that re-instantiates the object created with 
+##      'makeCacheMatrix' with a new matrix value, when called from outside 
+##      'makeCacheMatrix'.
+##  2) 'get()', a function that returns the object crated by 'makeCacheMatrix'.
+##  3) 'setinverse(solve)', a function that fills the content of the cache 
+##      (c_im1) with the inverse matrix of the matrix in the object created by 
+##      'makeCacheMatrix'.
+##  4) 'getinverse()', a function that returns the content of the cache (c_im1).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    c_im1 <- NULL
+    
+    set <- function(y = matrix()) {
+        x <<- y
+        c_im1 <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(solve) c_im1 <<- solve
+    
+    getinverse <- function() c_im1
+    
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
+##------------------------------------------------------------
+##  'cacheSolve', does three things:
+##  1) It checks the content of the cache (c_im2) calling 'x$getinverse' and
+##      determines if it has content, or it doesn't.
+##  2) If it doesn't (i.e., if it is NULL), it gets the matrix created by 
+##      'makeCacheMatrix' calling 'x$get()' and computes its inverse matrix.
+## NOTE:    The conditional (below) is an add on that determines if the inverse 
+##          matrix can be calculated. If it cannot be computed, it avoids R's 
+##          error message & debugging step and, instead, sends a friendly 
+##          message finishing the call.
+##  3) Lastly, it stores the computed inverse matrix into the cache (c_im2) and 
+##      returns it when called.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    c_im2 <- x$getinverse()
+    
+    if(!is.null(c_im2)) {
+        message("Getting cached data...")
+        return(c_im2)
+    }
+    
+    data <- x$get()
+    
+    
+    
+    if ( inherits( try( c_im2 <- solve(data, ...), silent=TRUE),  "try-error")){
+        print("Oops, the inverse matrix doesn't exist.")
+    } 
+    else {
+        x$setinverse(c_im2)
+        c_im2
+    }
 }
+##---------------------------------------------------------------
+
